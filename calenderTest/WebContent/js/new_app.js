96ef94fd64f021f8ab720fcc9c2f1501435757a2
andrew <andrew@192.168.0.76>
2016-09-07 13:40:16 +0800

    update on 2016/9/7

--- calenderTest/WebContent/js/new_app.js
+++ calenderTest/WebContent/js/new_app.js
@@ -1,135 +1,143 @@
+var dateInputExample = angular
+		.module('dateInputExample', [])
+		.controller(
+				'myDateController',
+				[
+						'$http','$location',
+						function($http,$location) {
+							var self = this;
 
-var dateInputExample = angular.module('dateInputExample', []).controller('DateController', [ '$scope', function($scope) {
-
-			var self = this;
-			// self.number = getWeeksInMonth(year,month+1).length ;
-
-			$scope.example = {
-				value : new Date(2013, 9, 22)
-			};
-
-		} ]);
-
-dateInputExample.controller('myDateController', ['$http', function($http) {
-	var self = this;
-	
-	self.date = new Date();
-	self.DB = 'LJ';
-	
-	self.year = self.date.getYear() + 1900;
-	self.month = self.date.getMonth();
-	
-	self.weekString = [ '日', '一', '二', '三', '四', '五', '六' ];
-	self.weekDays = getWeeksInMonth(self.year, self.month);
-	self.dateString =  dateFormat("yyyyMMdd",self.date);
-	
-	console.log(self.dateString);
-	
-	self.orderInfos = [ {
-		id : '20160715000001',
-		productID : 'A0010010001',
-		productName : 'PSC1812-N',
-		orderAmount : 10,
-		makedAmount : 5,
-		unit : 'pcs',
-		date : '20160901'
-	}, {
-		id : '20160722000015',
-		productID : 'A0010010004',
-		productName : 'PSC0801-NP',
-		orderAmount : 1000,
-		makedAmount : 500,
-		unit : 'pcs',
-		date : '20160905'
-	} ];
-	
-	// self.orderInfos = [] ;
-	
-	self.checkHaveDataOrNot = self.orderInfos.length == 0;
-
-	
-	
-	var getOrderInfos = function() {
-		return $http
-				.get('ManufacOrderServlet?type=ManufacOrder&date='+ self.dateString + '&DB='+ self.DB)
-				.then(
-						function(response) {
-							self.orderInfos = response.data;
+							self.params = $location.search();
+							self.dateString = self.params.date ;
 							
+							console.log(self.dateString);
+							
+							if(self.dateString === undefined ){
+								self.date = new Date();
+								self.year = self.date.getYear() + 1900;
+								self.month = self.date.getMonth();
+								self.dateString = dateFormat("yyyyMMdd", self.date);
+								$location.search('date', self.dateString);
+								console.log('year>>',self.year,'month>>',self.month);
+							}else{
+								self.year = parseInt(self.dateString.substring(0,4));
+								self.month = parseInt(self.dateString.substring(4,6))-1;								
+								console.log('year>>',self.year,'month>>',self.month);
+							}
+							
+							
+							
+							self.DB = 'LJ';
+							self.weekString = [ '日', '一', '二', '三', '四', '五',	'六' ];
+							self.weekDays = getWeeksInMonth(self.year,self.month);
+							
+							
+							
+
+							self.orderInfos = [ {
+								id : '20160715000001',
+								productID : 'A0010010001',
+								productName : 'PSC1812-N',
+								orderAmount : 10,
+								makedAmount : 5,
+								unit : 'pcs',
+								date : '20160901'
+							}, {
+								id : '20160722000015',
+								productID : 'A0010010004',
+								productName : 'PSC0801-NP',
+								orderAmount : 1000,
+								makedAmount : 500,
+								unit : 'pcs',
+								date : '20160905'
+							} ];
+
+							// self.orderInfos = [] ;
+
 							self.checkHaveDataOrNot = self.orderInfos.length == 0;
-							console.log(self.checkHaveDataOrNot);
-							console.log(self.orderInfos.length);
-							console.log(self.orderInfos);
-							
-						},
-						function(errResponse) {
-							console.log('Error while fetching notes');
-						});
-	};
 
-	getOrderInfos();
-	
-	self.filterOptions = function(day){ // 將傳入的參數收下來
-		return function(data) { // 將 data in dateCtrl.orderInfos 的 data 收下來
-			
-			// DateFormat("yyyyMMdd").format(day) >> dayString
-			var dayString = dateFormat("yyyyMMdd",day);			
-			return parseInt(dayString) == data.date ;
-		};
-		
-	};
-	
-	
-	self.holidayOrNot = function(dayString) {
+							var getOrderInfos = function() {
+								return $http
+										.get(
+												'ManufacOrderServlet?type=ManufacOrder&date='
+														+ self.dateString
+														+ '&DB=' + self.DB)
+										.then(
+												function(response) {
+													self.orderInfos = response.data;
 
-		if (dayString == '日' || dayString == '六') {
-			return {
-				'color' : 'red'
-			};
-		} else {
-			return {
-				'color' : 'black'
-			};
-		}
-	};
+													self.checkHaveDataOrNot = self.orderInfos.length == 0;
+													console.log(self.checkHaveDataOrNot);
+													console.log(self.orderInfos.length);
+													console.log(self.orderInfos);
 
-	self.dayInMonthOrNot = function(year, month, date) {
-		var firstDate = new Date(year, month, 1), 
-			lastDate = new Date(year, month + 1, 0);
+												},
+												function(errResponse) {
+													console.log('Error while fetching notes');
+												});
+							};
 
-		if (date >= firstDate && date <= lastDate) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
+							getOrderInfos();
 
+							self.filterOptions = function(day) { // 將傳入的參數收下來
+								return function(data) { // 將 data in
+														// dateCtrl.orderInfos 的
+														// data 收下來
 
-} ]);
+									// DateFormat("yyyyMMdd").format(day) >> dayString
+									var dayString = dateFormat("yyyyMMdd", day);
+									return parseInt(dayString) == data.date;
+								};
 
+							};
 
+							self.holidayOrNot = function(dayString) {
 
-function dateFormat(format,date){
-	var dayString ;
-	if(format==="yyyyMMdd")	dayString = date.toISOString().substring(0, 4)+date.toISOString().substring(5, 7)+date.toISOString().substring(8,10);
-	
-	return dayString ;
+								if (dayString == '日' || dayString == '六') {
+									return {
+										'color' : 'red'
+									};
+								} else {
+									return {
+										'color' : 'black'
+									};
+								}
+							};
+
+							self.dayInMonthOrNot = function(year, month, date) {
+								var firstDate = new Date(year, month, 1), lastDate = new Date(
+										year, month + 1, 0);
+
+								if (date >= firstDate && date <= lastDate) {
+									return true;
+								} else {
+									return false;
+								}
+							}
+
+						} ]);
+
+function dateFormat(format, date) {
+	var dayString;
+	if (format === "yyyyMMdd")
+		dayString = date.toISOString().substring(0, 4)
+				+ date.toISOString().substring(5, 7)
+				+ date.toISOString().substring(8, 10);
+
+	return dayString;
 }
-
-
 
 // note: month is 0 based, just like Dates in js
 function getWeeksInMonth(year, month) {
 	var weeks = [], weekDays = [], firstDate = new Date(year, month, 1), lastDate = new Date(
 			year, month + 1, 0), numDays = lastDate.getDate();
-	
+
 	var start = 1;
-	
+
 	// 7 - 當月一號為週幾
 	var end = 7 - firstDate.getDay();
 	while (start <= numDays) {
-		
+
 		weeks.push({
 			start : start,
 			end : end
@@ -162,6 +170,3 @@ function getWeeksInMonth(year, month) {
 	}
 	return weekDays;
 }
-
-
-
